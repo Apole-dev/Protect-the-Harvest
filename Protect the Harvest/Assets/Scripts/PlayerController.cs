747c3fd9e31f0730e17b921e444ffdbc888e0132
Eren <74319360+Apole-dev@users.noreply.github.com>
2024-02-04 23:49:34 +0300

    New

---
+++ Protect the Harvest/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if(Input.touchCount > 0)
+        {
+            var touch = Input.GetTouch(0);
+            var horizontalMove = touch.deltaPosition.x;
+            print("horizontalMove: " + horizontalMove);
+            var movement = new Vector3(horizontalMove, 0f, 0f) * (moveSpeed * Time.deltaTime);
+
+            transform.Translate(movement);
+        }
+    }
+
+}
